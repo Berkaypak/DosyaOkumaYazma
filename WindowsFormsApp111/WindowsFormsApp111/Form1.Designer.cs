9cbdf33a19d6bcbf69ad32fc67f8b2b57930d1cb
Berkay <berkaypakybs@gmail.com>
2024-12-08 18:04:34 +0300

    DosyaOkuma-Treeview
    Verilen bir dosya konumundaki metin belgesinde bir harfin kaç kere geçtiğini treeview olarak gösteren program

---
+++ WindowsFormsApp111/WindowsFormsApp111/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace WindowsFormsApp111
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(36, 44);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(165, 108);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(445, 26);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(290, 397);
+            this.treeView1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TreeView treeView1;
+    }
+}
+
---
+++ WindowsFormsApp111/WindowsFormsApp111/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp111
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<char> harfler = new List<char>()
+        {
+          ' ', 'a','b','c','ç','d','e','f','g','ğ','h','ı','i','j','k','l','m','n','o','ö','p',
+           'r','s','ş','t','u','ü','v','y','z'
+        };
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for(int i = 0; i < harfler.Count; i++)
+            {
+                treeView1.Nodes.Add(harfler[i].ToString());
+            }
+            StreamReader sr = new StreamReader("hikaye.txt");
+            while (!sr.EndOfStream)
+            {
+                string line = sr.ReadLine();
+                string[] dizi = line.Split(' ');
+                foreach (string d in dizi)
+                {
+                  string tl=  d.ToLower();
+                    foreach (char s in tl)
+                    {
+                        if (harfler.Contains(s))
+                        {
+                            int io = harfler.IndexOf(s);
+                            treeView1.Nodes[io].Nodes.Add("1");
+                        }
+                    }
+                }
+            }
+            foreach (TreeNode tn in treeView1.Nodes)
+            {
+                int sayac = 0;
+                foreach (TreeNode tn2 in tn.Nodes)
+                {
+                    sayac++;
+                }
+                tn.Nodes.Clear();
+                tn.Nodes.Add(sayac.ToString());
+            }
+        }
+    }
+}
